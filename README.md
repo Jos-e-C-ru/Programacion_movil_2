7f66a83ffbcae7a136c1ddf4afdb94f5382574d3
kick800 <144750060+kick800@users.noreply.github.com>
2023-10-05 19:11:03 -0600

    Update

--- README.md
+++
@@ -1,2 +0,0 @@
-# Programacion_movil_2
-Programacion movil 2 (Avances_1_Parcial)
---
+++ portafolio/Models/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace portafolio.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+
+        public int Edad {  get; set; }
+
+
+    }
+}
